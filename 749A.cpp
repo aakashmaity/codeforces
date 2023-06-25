620d52f903572f9399b4ea10c3abebfa4cd52409
AKASH MAITY <akash.maity.csbs25@heritageit.edu.in>
2023-06-26 02:23:06 +0530

    done

--- 749A.cpp
+++ 749A.cpp
@@ -1,3 +1,30 @@
-
-
 // https: // codeforces.com/problemset/problem/749/A
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int k;
+    // if number is even
+    if (n % 2 == 0)
+    {
+        k = n / 2;
+        cout << k << endl;
+        for (int i = 1; i <= k; i++)
+        {
+            cout << 2 << " ";
+        }
+    }
+    else // when odd
+    {
+        int k = (n / 2);
+        cout << k << endl;
+        for (int i = 1; i <= (k - 1); i++) // total no. of 2's is k-1
+        {
+            cout << 2 << " ";
+        }
+        cout << 3;
+    }
+}
