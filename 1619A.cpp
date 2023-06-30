5b30749e2da7d637036dad8ae24e6cba304ad6ad
AKASH MAITY <akash.maity.csbs25@heritageit.edu.in>
2023-07-01 04:12:41 +0530

    done

---
+++ 1619A.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string str;
+        cin>>str;
+        int n=str.size();
+        if(n%2!=0){
+            cout<<"NO"<<endl;
+            continue;
+        }
+        string str1=str.substr(0,(n/2));
+        string str2=str.substr(n/2);
+        if(str1==str2){
+            cout<<"YES"<<endl;
+        }
+        else{ 
+            cout<<"NO"<<endl;
+        }
+    }
+    return 0;
+}
